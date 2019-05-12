699dd3e6fceae0d4f84022f3b5cb22f0679475f4
PoziWorld <5961162+PoziWorld@users.noreply.github.com>
2019-05-12 13:24:53 -0700

    New: Transifex webhook to forward received notification to Telegram chat

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# PoziTone Webhooks
+# PoziWorld Webhooks
 
-Work-in-progress.
+Serverless webhooks for [PoziWorld](https://github.com/PoziWorld)'s own needs:
+- [Transifex](https://www.transifex.com/PoziWorld/public/):
+  
+  Receives notifications about the following events in the projects and forwards the notifications to a specified Telegram chat:
+  - Translation completed
+  - Review completed
+  - Fill-up completed
--- handlers/transifex.js
+++ handlers/transifex.js
@@ -1,7 +1,12 @@
+const superagent = require( 'superagent' );
+
 const responseUtils = require( '../utils/response' );
 const typeUtils = require( '../utils/type' );
 
 const statusCode = require( '../model/status-code' );
+
+const TEXT_PLACEHOLDER = '$TEXT';
+const telegramUrl = `https://api.telegram.org/bot${ process.env.BOT_TOKEN }/sendMessage?chat_id=${ process.env.CHAT_ID }&text=${ TEXT_PLACEHOLDER }`;
 
 module.exports = handleTransifex;
 
@@ -18,7 +23,7 @@ module.exports = handleTransifex;
  * @param res
  */
 
-function handleTransifex( req, res ) {
+async function handleTransifex( req, res ) {
   const { body } = req;
   const { project } = body;
   const {
@@ -29,6 +34,55 @@ function handleTransifex( req, res ) {
 
     // @todo i18n
     responseUtils.markFailed( res, statusCode.BAD_REQUEST, 'Missing required parameter "project".' );
+  let result = null;
 
-  response.json( output );
+  try {
+    result = await forwardTimelineEvent( output );
+  }
+  catch ( e ) {
+    // @todo
+  }
+
+  response.json( {
+    ...output,
+    ...result,
+  } );
+}
+
+/**
+ * Send the webhook info to the specified Telegram chat.
+ *
+ * @param {Object} data - New Transifex project's timeline event.
+ * @return {Object}
+ */
+
+async function forwardTimelineEvent( data ) {
+  const stringifiedData = JSON.stringify( data );
+  const url = telegramUrl.replace( TEXT_PLACEHOLDER, stringifiedData );
+  let response = {};
+
+  try {
+    const {
+      body: {
+        ok,
+        result: {
+          // eslint-disable-next-line camelcase
+          message_id: messageId,
+        },
+      },
+    } = await superagent.get( url );
+
+    response = {
+      ok,
+      messageId,
+    };
+  }
+  catch ( e ) {
+    response = {
+      ok: false,
+      message: e,
+    };
+  }
+
+  return response;
 }
